c8ea3563c95e0ebe545b794faaa9cffcf1862939
hnxyhcwdl1003@163.com <hnxyhcwdl1003@163.com>
2018-05-09 18:16:52 +0800

    性能测试

--- src/test/java/com/github/vindell/ftpclient/FTPPooledResourceClientUploadTest.java
+++ src/test/java/com/github/vindell/ftpclient/FTPPooledResourceClientUploadTest.java
@@ -1,23 +1,34 @@
 package com.github.vindell.ftpclient;
 
 import java.io.File;
-import java.io.FileInputStream;
+import java.util.UUID;
 
+import org.apache.commons.io.FilenameUtils;
+import org.databene.contiperf.PerfTest;
+import org.databene.contiperf.junit.ContiPerfRule;
 import org.junit.FixMethodOrder;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 @FixMethodOrder(MethodSorters.JVM) 
 public class FTPPooledResourceClientUploadTest extends FTPPooledResourceClientTest {
 	
+	@Rule
+	public ContiPerfRule rule = new ContiPerfRule();
+	
 	/**
  	 * 向ftp写文件(数据)
  	 */
 	@Test
+	@PerfTest(invocations = 300,threads = 40)  
  	public void uploadFile() {
  		try {
- 			File localFile = new File("E:\\第九套广播体操.mp4");
  			
- 			ftpClient.upload(localFile,"tst.mp4");
+ 			File localFile = new File("E:\\第九套广播体操.mp4");
+ 			String filename = UUID.randomUUID().toString() + "." + FilenameUtils.getExtension(localFile.getName());
+ 			
+ 			ftpClient.upload(localFile, filename);
+ 			/*
  			ftpClient.upload(new FileInputStream(localFile),"tst2.mp4");
  			ftpClient.upload("E:\\第九套广播体操.mp4","tst1.mp4");
  			ftpClient.upload(new StringBuilder(fileContent),"tst1.txt");
@@ -35,7 +46,7 @@ public class FTPPooledResourceClientUploadTest extends FTPPooledResourceClientTe
  			
  			//部分ftp服务异常
  			ftpClient.uploadByChannel(localFile,"tst.mp4");
- 			ftpClient.uploadByChannel(localFile,"20063586/20160118","tst.mp4");
+ 			ftpClient.uploadByChannel(localFile,"20063586/20160118","tst.mp4");*/
  			
  		} catch (Exception e) {
  			e.printStackTrace();
--- src/test/java/com/github/vindell/ftpclient/FTPResourceClientUploadTest.java
+++ src/test/java/com/github/vindell/ftpclient/FTPResourceClientUploadTest.java
@@ -3,16 +3,23 @@ package com.github.vindell.ftpclient;
 import java.io.File;
 import java.io.FileInputStream;
 
+import org.databene.contiperf.PerfTest;
+import org.databene.contiperf.junit.ContiPerfRule;
 import org.junit.FixMethodOrder;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 @FixMethodOrder(MethodSorters.JVM) 
 public class FTPResourceClientUploadTest extends FTPResourceClientTest {
 	
+	@Rule
+	public ContiPerfRule rule = new ContiPerfRule();
+	 
 	/**
  	 * 向ftp写文件(数据)
  	 */
 	@Test
+	@PerfTest(invocations = 300,threads = 40)  
  	public void uploadFile() {
  		try {
  			File localFile = new File("E:\\第九套广播体操.mp4");
